8df8a77bcbe9ffba1cdaaa9bdbcd8383a7d50148
bcannon95 <brock.cannon@hotmail.com>
2022-08-06 10:03:20 +1000

    Awards  return

--- src/pages/Awards.js
+++ src/pages/Awards.js
@@ -18,43 +18,95 @@ export default function Awards(props) {
         ;
       }
       
-    return (
-        <>
-            {/* <div className='d-flex flex-wrap'> */}
-            <div>
-                <h2 className="h2-about-us">My Analytics</h2>
-                <div>
-                <p className="p1-about-us">Movies Viewed: {counter}</p>
-                <p className="p1-about-us">Average Rating: {result}</p>
-                {() => {
-                    if(counter >= 20) {
-                        return <img className="bbuster" src={gold} />
-                        }
-                        if(counter >= 10) {
-                        return <img className="bbuster" src={silver} />
-                        }
-                        if(counter >= 5) {
-                        return <img className="bbuster" src={bronze} />
-                        }
-                }}
-                {/* {props.movieList.map(movie => {
-                    console.log(counter);
-                    console.log(movie.viewed);
-                    console.log(props.movieList);
-                    console.log(props.movieList.length);
-                    if(counter >= 20) {
-                    return <img className="bbuster" src={gold} />
-                    }
-                    if(counter >= 10) {
-                    return <img className="bbuster" src={silver} />
-                    }
-                    if(counter >= 5) {
-                    return <img className="bbuster" src={bronze} />
-                    }
+    // return (
+    //     <>
+    //         {/* <div className='d-flex flex-wrap'> */}
+    //         <div>
+    //             <h2 className="h2-about-us">My Analytics</h2>
+    //             <div>
+    //             <p className="p1-about-us">Movies Viewed: {counter}</p>
+    //             <p className="p1-about-us">Average Rating: {result}</p>
+
+    //                 if(counter >= 20) {
+    //                     return <img className="bbuster" src={gold} />
+    //                     }
+    //                     if(counter >= 10) {
+    //                     return <img className="bbuster" src={silver} />
+    //                     }
+    //                     if(counter >= 5) {
+    //                     return <img className="bbuster" src={bronze} />
+    //                     }
+                
+    //             {/* {props.movieList.map(movie => {
+    //                 console.log(counter);
+    //                 console.log(movie.viewed);
+    //                 console.log(props.movieList);
+    //                 console.log(props.movieList.length);
+    //                 if(counter >= 20) {
+    //                 return <img className="bbuster" src={gold} />
+    //                 }
+    //                 if(counter >= 10) {
+    //                 return <img className="bbuster" src={silver} />
+    //                 }
+    //                 if(counter >= 5) {
+    //                 return <img className="bbuster" src={bronze} />
+    //                 }
+    //             }
+    //             )} */}
+    //         </div>
+    //         </div>
+    //     </>
+    // )
+
+
+            if(counter >= 20) {
+                return (             
+                    <div>
+                        <h2 className="h2-about-us">My Analytics</h2>
+                    <div>
+                    <p className="p1-about-us">Movies Viewed: {counter}</p>
+                    <p className="p1-about-us">Average Rating: {result}</p>
+                    <h2 className="h2-about-us">View Awards</h2>
+                        <img className="bbuster" src={gold} />
+                </div>
+                </div>
+                )
                 }
-                )} */}
-            </div>
-            </div>
-        </>
-    )
+                if(counter >= 10) {
+                return (             
+                    <div>
+                        <h2 className="h2-about-us">My Analytics</h2>
+                    <div>
+                    <p className="p1-about-us">Movies Viewed: {counter}</p>
+                    <p className="p1-about-us">Average Rating: {result}</p>
+                    <h2 className="h2-about-us">View Awards</h2>
+                        <img className="bbuster" src={silver} />
+                </div>
+                </div>
+                )
+                }
+                if(counter >= 5) {
+                return (             
+                    <div>
+                        <h2 className="h2-about-us">My Analytics</h2>
+                    <div>
+                    <p className="p1-about-us">Movies Viewed: {counter}</p>
+                    <p className="p1-about-us">Average Rating: {result}</p>
+                    <h2 className="h2-about-us">View Awards</h2>
+                        <img className="bbuster" src={bronze} />
+                </div>
+                </div>
+                )
+                } else {
+                return (             
+                    <div>
+                        <h2 className="h2-about-us">My Analytics</h2>
+                    <div>
+                    <p className="p1-about-us">Movies Viewed: {counter}</p>
+                    <p className="p1-about-us">Average Rating: {result}</p>
+                </div>
+                </div>
+                )
+                }
+
 }
