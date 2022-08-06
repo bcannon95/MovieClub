e2a2ce87d1d8ebef36ace27e5d0d2588ca9f71a2
bcannon95 <brock.cannon@hotmail.com>
2022-08-06 10:36:30 +1000

    app tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import MovieCard from './components/MovieCard';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+// Test MovieCard Rating
+
+test('MovieCard Rating', (props) => {
+  render(<MovieCard />);
+  const movieRating = props.movieList[1];
+  expect(movieRating).toBe(8.6);
+});
+
+// Test MovieCard Year
+
+test('MovieCard Year', (props) => {
+  render(<MovieCard />);
+  const movieYear = props.movieList[year]
+  expect(movieYear).toBe(1997);
 });
--- src/pages/Awards.js
+++ src/pages/Awards.js
@@ -5,113 +5,72 @@ import gold from '../components/gold.png'
 import AwardsCard from '../components/AwardsCard'
 
 export default function Awards(props) {
-      
-      let counter = 0;
-      let result = []
-      let sum = 0;
-      for (let i = 0; i < props.movieList.length; i++) {
-        if (props.movieList[i].viewed === 'yes') 
-        counter++
-        sum += props.movieList[i].rating
-        const curr = sum / counter
-        result[i] = curr
-        ;
-      }
-      
-    // return (
-    //     <>
-    //         {/* <div className='d-flex flex-wrap'> */}
-    //         <div>
-    //             <h2 className="h2-about-us">My Analytics</h2>
-    //             <div>
-    //             <p className="p1-about-us">Movies Viewed: {counter}</p>
-    //             <p className="p1-about-us">Average Rating: {result}</p>
-
-    //                 if(counter >= 20) {
-    //                     return <img className="bbuster" src={gold} />
-    //                     }
-    //                     if(counter >= 10) {
-    //                     return <img className="bbuster" src={silver} />
-    //                     }
-    //                     if(counter >= 5) {
-    //                     return <img className="bbuster" src={bronze} />
-    //                     }
-                
-    //             {/* {props.movieList.map(movie => {
-    //                 console.log(counter);
-    //                 console.log(movie.viewed);
-    //                 console.log(props.movieList);
-    //                 console.log(props.movieList.length);
-    //                 if(counter >= 20) {
-    //                 return <img className="bbuster" src={gold} />
-    //                 }
-    //                 if(counter >= 10) {
-    //                 return <img className="bbuster" src={silver} />
-    //                 }
-    //                 if(counter >= 5) {
-    //                 return <img className="bbuster" src={bronze} />
-    //                 }
-    //             }
-    //             )} */}
-    //         </div>
-    //         </div>
-    //     </>
-    // )
-
-            if(counter >= 20) {
-                return (             
-                    <div>
-                        <h2 className="h2-about-us">My Analytics</h2><br></br>
-                    <div>
-                    <p className="p1-about-us">Movies Viewed: {counter}</p><br></br>
-                    <p className="p1-about-us">Average Rating: {result}</p><br></br>
-                    <div>
-                    <h2 className="h2-about-us">View Awards</h2><br></br>
-                        <img className="awards" src={gold} /><br></br>
-                    </div>
-                </div>
-                </div>
-                )
-                }
-                if(counter >= 10) {
-                return (             
-                    <div>
-                        <h2 className="h2-about-us">My Analytics</h2><br></br>
-                    <div>
-                    <p className="p1-about-us">Movies Viewed: {counter}</p><br></br>
-                    <p className="p1-about-us">Average Rating: {result}</p><br></br>
-                    <div>
-                    <h2 className="h2-about-us">View Awards</h2><br></br>
-                        <img className="awards" src={silver} /><br></br>
-                    </div>
-                </div>
-                </div>
-                )
-                }
-                if(counter >= 5) {
-                return (             
-                    <div>
-                        <h2 className="h2-about-us">My Analytics</h2><br></br>
-                    <div>
-                    <p className="p1-about-us">Movies Viewed: {counter}</p><br></br>
-                    <p className="p1-about-us">Average Rating: {result}</p><br></br>
-                    <div>
-                    <h2 className="h2-about-us">View Awards</h2><br></br>
-                        <img className="awards" src={bronze} /><br></br>
-                    </div>
-                </div>
-                </div>
-                )
-                } else {
-                return (             
-                    <div>
-                        <h2 className="h2-about-us">My Analytics</h2><br></br>
-                    <div>
-                    <p className="p1-about-us">Movies Viewed: {counter}</p><br></br>
-                    <p className="p1-about-us">Average Rating: {result}</p><br></br>
-                </div>
-                </div>
-                )
-                }
+    
+    let counter = 0;
+    let result = []
+    let sum = 0;
+    for (let i = 0; i < props.movieList.length; i++) {
+    if (props.movieList[i].viewed === 'yes') 
+    counter++
+    sum += props.movieList[i].rating
+    const curr = sum / counter
+    result[i] = curr
+    ;
+    }
+    if(counter >= 20) {
+        return (             
+            <div>
+                <h2 className="h2-about-us">My Analytics</h2><br></br>
+            <div>
+            <p className="p1-about-us">Movies Viewed: {counter}</p><br></br>
+            {/* <p className="p1-about-us">Average Rating: {result}</p><br></br> */}
+            <div>
+            {/* <h2 className="h2-about-us">View Awards</h2><br></br> */}
+                <img className="awards" src={gold} /><br></br>
+            </div>
+        </div>
+        </div>
+        )
+        }
+        if(counter >= 10) {
+        return (             
+            <div>
+                <h2 className="h2-about-us">My Analytics</h2><br></br>
+            <div>
+            <p className="p1-about-us">Movies Viewed: {counter}</p><br></br>
+            {/* <p className="p1-about-us">Average Rating: {result}</p><br></br> */}
+            <div>
+            {/* <h2 className="h2-about-us">View Awards</h2><br></br> */}
+                <img className="awards" src={silver} /><br></br>
+            </div>
+        </div>
+        </div>
+        )
+        }
+        if(counter >= 5) {
+        return (             
+            <div>
+                <h2 className="h2-about-us">My Analytics</h2><br></br>
+            <div>
+            <p className="p1-about-us">Movies Viewed: {counter}</p><br></br>
+            {/* <p className="p1-about-us">Average Rating: {result}</p><br></br> */}
+            <div>
+            {/* <h2 className="h2-about-us">View Awards</h2><br></br> */}
+                <img className="awards" src={bronze} /><br></br>
+            </div>
+        </div>
+        </div>
+        )
+        } else {
+        return (             
+            <div>
+                <h2 className="h2-about-us">My Analytics</h2><br></br>
+            <div>
+            <p className="p1-about-us">Movies Viewed: {counter}</p><br></br>
+            {/* <p className="p1-about-us">Average Rating: {result}</p><br></br> */}
+        </div>
+        </div>
+        )
+        }
 
 }
